cf3302b4d5eb80240cdd873b027732170b47728f
fredyu <xiaoliyu@gmail.com>
2023-02-23 23:32:35 -0500

    Add files via upload
    INit

---
+++ commands.js
@@ -0,0 +1,2 @@
+Office.onReady((function(){console.log("commands ready")})),Office.context.platform!==Office.PlatformType.PC&&null!=Office.context.platform||Office.actions.associate("onNewMessageComposeHandler",(function(e){!function(e){Office.context.mailbox.item.subject.setAsync("Set by an event-based add-in!",{asyncContext:e},(function(e){e.status!==Office.AsyncResultStatus.Succeeded&&console.error("Failed to set subject: "+JSON.stringify(e.error)),e.asyncContext.completed()}))}(e)}));
+//# sourceMappingURL=commands.js.map
